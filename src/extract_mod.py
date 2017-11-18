c50f5c7cdbb410b25c960163e2f0dd782b123d8a
sam <samuel.gregoire@mail.mcgill.ca>
2017-11-18 13:39:14 -0500

    Added the option to specify the wanted column indices in the function call

--- src/extract_mod.py
+++ src/extract_mod.py
@@ -2,27 +2,28 @@
 
 import csv
 
-def extract(input_file, output_file=""):
+def extract(input_file, output_file="", selected_columns = []):
     with open(input_file) as f:
         reader = csv.reader(f)
         header_row = next(reader)
-    
-        # We print the header row with the associated indices
-        for i in range(0,len(header_row)):
-            print(str(i) + ": " + header_row[i])
+        
+        # We prompt user for columns if none are provided
+        if len(selected_columns) == 0:
+            # We print the header row with the associated indices
+            for i in range(0,len(header_row)):
+                print(str(i) + ": " + header_row[i])
 
-        # The user is prompted to select the columsn he wants to extract
-        selected_columns = []
-        selection = input("Enter the index of a column you wish to extract, enter anything that is not a number when done: ")
+            # The user is prompted to select the columsn he wants to extract
+            selection = input("Enter the index of a column you wish to extract, enter anything that is not a number when done: ")
     
-        # We keep asking as long as the input is valid
-        while selection.isdigit():
-            if int(selection) >= len(header_row):
-                print("This index is out of bounds, make a valid selection")
+            # We keep asking as long as the input is valid
+            while selection.isdigit():
+                if int(selection) >= len(header_row):
+                    print("This index is out of bounds, make a valid selection")
 
-            else:
-                selected_columns.append(int(selection))
-                selection = input("Enter the index of a column you wish to extract, enter anything that is not a number when done: ")
+                else:
+                    selected_columns.append(int(selection))
+                    selection = input("Enter the index of a column you wish to extract, enter anything that is not a number when done: ")
 
         # If an output file is provided, we write the selected columns to it (only supports csv)
         if output_file != "":
