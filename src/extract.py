56c7d4d8b57cc8430985a059c1c7f25a6e79b500
sam <samuel.gregoire@mail.mcgill.ca>
2017-11-18 10:28:16 -0500

    Added some comments and minor formatting

--- src/extract.py
+++ src/extract.py
@@ -3,8 +3,10 @@
 import csv
 import sys
 
+# Put in the path to the file you want in the arguments
 filepath = sys.argv[1]
+
 with open(filepath) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    print(header_row)   #This line prints the header row
